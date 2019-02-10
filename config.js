96cf5cfa75181ab7e2a92627752dee379387e8b5
Worthen <dworthen@wsu.edu>
2019-02-09 19:51:48 -0800

    initial commit

---
+++ config.js
@@ -0,0 +1,44 @@
+"use strict";
+
+module.exports = {
+  url: "http://localhost:8000",
+  title: "Blog by Derek Worthen",
+  subtitle: "ASP.NET Core, Blazor and TypeScript.",
+  copyright: "© All rights reserved.",
+  disqusShortname: "",
+  postsPerPage: 4,
+  googleAnalyticsId: "UA-73379983-2",
+  menu: [
+    {
+      label: "Articles",
+      path: "/"
+    },
+    // {
+    //   label: "About me",
+    //   path: "/pages/about"
+    // },
+    // {
+    //   label: "Contact me",
+    //   path: "/pages/contacts"
+    // },
+    {
+      label: "Tags",
+      path: "/tags"
+    }
+  ],
+  author: {
+    name: "Derek Worthen",
+    photo: "/photo.jpg",
+    bio: "ASP.NET Core, Blazor and TypeScript.",
+    contacts: {
+      // email: "#",
+      // telegram: "#",
+      twitter: "#",
+      github: "dworthen",
+      rss: "rss.xml",
+      facebook: "https://facebook.com/derek.worthen",
+      instagram: "https://www.instagram.com/derekworthen/"
+      // vkontakte: "#"
+    }
+  }
+};
